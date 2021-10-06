49f302fbb51fe45e1e2563f5158ebcd82427a01f
CWei88 <chenwei5lv@gmail.com>
2021-10-06 17:17:51 +0800

    Fixed errors in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
-
+import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
-
+    
 ]
 
-export default router;
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
