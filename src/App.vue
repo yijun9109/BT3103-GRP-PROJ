6a1870686f0aecc9b7aa52c70dbf6150fa138554
aucjl <69624115+aucjl@users.noreply.github.com>
2021-10-12 18:25:42 +0800

    minor edits to navbar still need to figure out how to get icon to show

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <router-link to= "/Home">Home</router-link>
     <router-link to= "/Tips">Tips</router-link>
     <router-link to= "/ShoppingList">Shopping List</router-link>
+
+    <!-- <div id="logo">
+        <a href="/App">
+            <div id="logo-img" alt="Logo image"></div>
+        </a>
+    </div> -->
+
+    <!-- need to add a button for login and also display the user name when logged in under "nav" -->
+
   </div>
   <router-view />
 </template>
@@ -20,13 +29,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
 }
 
 #nav {
-  padding: 30px;
-  text-align: center;
+  padding-top: 30px;
+  padding-bottom:30px;
+  padding-right: 50px;
+  text-align: right;
   background: rgb(241, 220, 150);
+  margin-top: -10px;
+  margin-left: -10px;
+  width: 100%;
+}
+
+#logo-img {
+  background: url('/src/assets/icon.png') no-repeat;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  height: 100%;
+  /* width: 20%; */
+  /* width: 30px;
+  height: 30px; */
 }
 
 #nav a {
